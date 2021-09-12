f804199c221bd37e0aa7264042b2cba6a59c2a7c
pradhumnts <62463488+pradhumnts@users.noreply.github.com>
2021-09-12 13:08:25 +0530

    Update settings.py

--- rentaltake/settings.py
+++ rentaltake/settings.py
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = (
+os.path.join(BASE_DIR, 'static'),
+)
